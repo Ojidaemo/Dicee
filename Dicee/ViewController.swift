44f70013894aab7205244121e7f63ce8b8136ab2
Ojidaemo <vit.martsinovich@gmail.com>
2023-01-22 17:52:48 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Vitali Martsinovich on 2023-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
